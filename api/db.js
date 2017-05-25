d10c925c932bb0008c7228b43add26fd6c7ee71c
srjahir32 <srjahir32@gmail.com>
2017-05-25 18:39:49 +0530

    update package.json and first page

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var config = require('./config/config.js')
-var mongoUrl = process.env.MONGO_URL;
+var mongoUrl = process.env.MONGODB_URI;
 
 exports.connect = function(callback) {
     MongoClient.connect(mongoUrl, function(err, db) {
